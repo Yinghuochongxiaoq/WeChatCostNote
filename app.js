a8d5003d1724874fd7708b7c4f1523fbf88ad98d
FreshMan <qinbocai@sina.cn>
2019-06-24 22:43:47 +0800

    添加时间组件

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //app.js
 App({
-  onLaunch: function () {
-    
-  },
-  globalData: {
-    userInfo: null,
-    // api: 'https://api.aivabc.com:8080/api',
-    api: 'http://localhost:8004/api',
-  }
+    onLaunch: function() {
+
+    },
+    globalData: {
+        userInfo: null,
+        // api: 'https://api.aivabc.com:8080/api',
+        api: 'http://localhost:8001/api',
+    }
 })
