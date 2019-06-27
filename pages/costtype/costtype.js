d4f9ab3efbe7c2fe468518d7653ef087666dd987
FreshMan <qinbocai@sina.cn>
2019-06-27 18:12:15 +0800

    添加类型修改

--- pages/costtype/costtype.js
+++ pages/costtype/costtype.js
@@ -1,8 +1,70 @@
+var app = getApp();
 Page({
-    onShareAppMessage() {
-        return {
-            title: 'iView Weapp',
-            imageUrl: 'https://file.iviewui.com/iview-weapp-logo.png'
-        };
+    data: {
+        spendType: 0,
+        costTypemultiArray: [],
+        costTypeObjectMultiArray: []
     },
+    onLoad: function () {
+        this.getAllCostType()
+    },
+    handlerSpendTypeChange: function ({
+        detail
+    }) {
+        this.setData({
+            spendType: detail.key
+        })
+        this.changeSpendType(detail.key)
+    },
+    changeSpendType: function (spendType) {
+        var typeList = [];
+        for (var i = 0; i < this.data.costTypeObjectMultiArray.length; i++) {
+            var temp = this.data.costTypeObjectMultiArray[i];
+            if (temp.spendType != spendType) continue;
+            typeList.push({
+                id: temp.id,
+                name: temp.name
+            });
+        }
+        this.setData({
+            costTypemultiArray: typeList
+        })
+    },
+    getAllCostType: function () {
+        var self = this;
+        wx.request({
+            url: app.globalData.api + '/CostNote/GetCostChannelType',
+            data: {
+                token: app.globalData.userInfo.token
+            },
+            method: 'GET',
+            success: function (res) {
+                if (res.data.resultCode == 0) {
+                    self.setData({
+                        costTypeObjectMultiArray: res.data.data.costTypeData
+                    })
+                    self.changeSpendType(self.data.spendType)
+                } else {
+                    wx.showToast({
+                        title: res.data.message,
+                        icon: 'none',
+                        duration: 2000
+                    })
+                }
+            }
+        })
+    },
+    handleNavigateClick:function(){
+        wx.navigateTo({
+            url: "/pages/costtype/edit?id=0"
+         });
+    },
+    onShareAppMessage() {     
+        return {    
+            title: '记录生活印迹',
+            desc: '在这里记录你的每一点一滴~',
+            path: 'pages/index/index',
+            imageUrl: '/images/share.jpg'   
+        }   
+    }
 });
---
+++ pages/costtype/edit.js
@@ -0,0 +1,163 @@
+var app = getApp();
+Page({
+    data: {
+        model: {
+            id: 0,
+            name: '',
+            sort: 0,
+            spendType: -1
+        },
+        spendTypeList: ['请选择', '支出', '收入'],
+        spendTypeIds: [-1, 0, 1],
+        spendTypeIndex: 0,
+        isSaving: false,
+        canDelete: false,
+        visibleDelete: false,
+        deleteActions: [{
+            name: '删除',
+            color: '#ed3f14'
+        }]
+    },
+    onLoad: function (options) {
+        var id = options.id;
+        if (id && id > 0) {
+            this.getCostTypeModel(id)
+        }
+    },
+    bindSpendTypeChange: function (e) {
+        this.setData({
+            spendTypeIndex: e.detail.value,
+            "model.spendType": this.data.spendTypeIds[e.detail.value]
+        })
+    },
+    changeSort: function (e) {
+        this.setData({
+            "model.sort": e.detail.detail.value
+        })
+    },
+    getCostTypeModel: function (id) {
+        var self = this;
+        wx.request({
+            url: app.globalData.api + '/CostNote/GetCostTypeModel',
+            data: {
+                token: app.globalData.userInfo.token,
+                id: id
+            },
+            method: 'GET',
+            success: function (res) {
+                if (res.data.resultCode == 0) {
+                    self.setData({
+                        model: res.data.data,
+                        canDelete: true,
+                        spendTypeIndex: res.data.data.spendType + 1
+                    })
+                } else {
+                    wx.showToast({
+                        title: res.data.message,
+                        icon: 'none',
+                        duration: 2000
+                    })
+                }
+            }
+        })
+    },
+    changeName: function (e) {
+        this.setData({
+            "model.name": e.detail.detail.value
+        })
+    },
+    handleSaveClick: function (e) {
+        var self = this;
+        wx.request({
+            url: app.globalData.api + '/CostNote/SaveTypeInfo',
+            data: {
+                token: app.globalData.userInfo.token,
+                id: self.data.model.id,
+                name: self.data.model.name,
+                sort: self.data.model.sort,
+                spendType: self.data.model.spendType
+            },
+            method: 'GET',
+            success: function (res) {
+                if (res.data.resultCode == 0) {
+                    self.setData({
+                        model: {
+                            id: 0,
+                            name: '',
+                            sort: 0,
+                            spendType: -1
+                        },
+                        canDelete: false,
+                        spendTypeIndex: 0
+                    })
+                    wx.showToast({
+                        title: '保存成功',
+                        icon: 'success',
+                        duration: 2000
+                    })
+                } else {
+                    wx.showToast({
+                        title: res.data.message,
+                        icon: 'none',
+                        duration: 2000
+                    })
+                }
+            }
+        })
+    },
+    handleOpenDelete: function () {
+        this.setData({
+            visibleDelete: true
+        });
+    },
+    handleCancelDelete: function () {
+        this.setData({
+            visibleDelete: false
+        });
+    },
+    handleDeleteCostType: function () {
+        const action = [this.data.deleteActions];
+        action[0].loading = true;
+        this.setData({
+            deleteActions: action
+        });
+
+        var self = this;
+        wx.request({
+            url: app.globalData.api + '/CostNote/DeleteCostTypeModel',
+            data: {
+                token: app.globalData.userInfo.token,
+                id: self.data.model.id
+            },
+            method: 'GET',
+            success: function (res) {
+                if (res.data.resultCode == 0) {
+                    action[0].loading = false;
+                    self.setData({
+                        visibleDelete: false,
+                        deleteActions: action,
+                        model: {
+                            id: 0,
+                            name: '',
+                            sort: 0,
+                            spendType: -1
+                        },
+                        canDelete: false,
+                        spendTypeIndex: 0
+                    });
+                    wx.showToast({
+                        title: '删除成功',
+                        icon: 'success',
+                        duration: 2000
+                    })
+                } else {
+                    wx.showToast({
+                        title: res.data.message,
+                        icon: 'none',
+                        duration: 2000
+                    })
+                }
+            }
+        })
+    }
+})
